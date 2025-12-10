fd7b001111a40bb2b195b0d76bcef93a475a3dcb
FridaJGM <150403231+FridaJGM@users.noreply.github.com>
2025-12-09 18:01:14 -0600

    corregi colores

--- app/src/main/java/com/example/musicapp/ui/components/AuthComponents.kt
+++ app/src/main/java/com/example/musicapp/ui/components/AuthComponents.kt
@@ -1,2 +1,71 @@
 package com.example.musicapp.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.example.musicapp.ui.theme.*
+
+@Composable
+fun AuthTextField(
+    label: String,
+    placeholder: String,
+    isPassword: Boolean = false
+) {
+    var text by remember { mutableStateOf("") }
+
+    Column( modifier = Modifier
+        .fillMaxSize())
+    {
+        Text(
+            text = label,
+            color = DarkBlue,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(start = 4.dp, bottom = 4.dp)
+        )
+
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            placeholder = { Text(placeholder, color= PlaceholderGray)},
+            singleLine = true,
+            visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = InputDark,
+                unfocusedContainerColor = InputDark,
+                cursorColor = Bluey,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            shape = RoundedCornerShape(20.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp)
+        )
+    }
+}
+
+@Composable
+fun BlueButton(text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(55.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Bluey
+        ),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Text(text, fontWeight = FontWeight.Bold, color = PureWhite)
+    }
+}
--- app/src/main/java/com/example/musicapp/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/musicapp/ui/screens/LoginScreen.kt
@@ -1,6 +1,5 @@
-package com.example.musicapp.ui.login
+package com.example.musicapp.ui.screens
 
-import android.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
