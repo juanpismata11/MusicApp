e3a55c82f94c6397beb9a8017985dc33e84338ca
jmata-tech <paulinaorozcolsb@email.com>
2025-12-09 19:41:13 -0600

    Creacion de homescreen con internet y gradel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    kotlin("plugin.serialization") version "2.0.21"
+
+    id("com.google.devtools.ksp") version "2.2.20-2.0.4"
+    id("de.jensklingenberg.ktorfit") version "2.6.4"
 }
 
 android {
@@ -57,4 +62,23 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.3")
+    // Icons
+    implementation("org.jetbrains.compose.material:material-icons-extended:1.7.3")
+// Ktorfit
+    implementation("de.jensklingenberg.ktorfit:ktorfit-lib:2.6.4")
+
+// Ktor Client
+    implementation("io.ktor:ktor-client-core:3.3.1")
+    implementation("io.ktor:ktor-client-content-negotiation:3.3.1")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.3.1")
+
+// Coil 3
+    implementation("io.coil-kt.coil3:coil-compose:3.1.0")
+    implementation("io.coil-kt.coil3:coil-network-ktor3:3.1.0")
+
+    implementation("com.russhwolf:multiplatform-settings-no-arg:1.3.0")
+
 }
